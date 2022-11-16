5992b8f83951b518b0d7a16e32cf57724351412a
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-11-16 13:30:30 +0300

    added color

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ const random = require('canvas-sketch-util/random');
 const settings = {
   dimensions: [1080, 1080],
 };
-
+//declare params from array inside sketch function
 const sketch = ({ context, width, height }) => {
-  let x, y, w, h;
-  //let angle, rx, ry;
-  const num = 20;
-  const degrees = -30;
+  let x, y, w, h, fill, stroke;
+  //set number of rectangles and angle 
+  const num = 22;
+  const degrees = -25;
 
+ // creat array to store random output
   const rects = [];
 
+// for loop set that runs according to the value of const num  
   for (let i = 0; i < num; i++) {
       x = random.range(0, width);
       y = random.range(0, height);
       w = random.range(200,600);
       h = random.range(40,200);
 
-      rects.push({ x, y, w, h });
+      fill= 'blue';
+      stroke= 'black';
+//push into array
+      rects.push({ x, y, w, h, fill, stroke });
   }
 
   //render function
@@ -28,15 +33,20 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+//set function of the array with for each
     rects.forEach(rect =>{
-      const {x, y, w, h} = rect;
+
+// destructure   
+      const {x, y, w, h, fill, stroke} = rect;
 
       context.save();
       context.translate(x, y);
-      context.strokeStyle = `blue`;
+      context.strokeStyle = stroke;
+      context.fillStyle = fill;
     
       drawSkewedRect({context, w, h, degrees});
       context.stroke();
+      context.fill();
 
 
       context.restore();
