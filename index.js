eba007982925157e6130f0422f38d9475585e8b9
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-11-16 08:56:53 +0300

    Added blue rectangle - saving before I break it

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const settings = {
 };
 
 const sketch = () => {
+  let x, y, w, h;
+
+  //render function
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+
+    x = width * 0.5;
+    y = height * 0.5;
+    w = width * 0.5;
+    h = height * 0.1;
+
+    context.save();
+    context.translate(x, y);
+
+    context.strokeStyle = `blue`;
+    context.strokeRect(x * -0.5, y * -0.5, w, h);
+
+    contect.restore();
   };
 };
 
