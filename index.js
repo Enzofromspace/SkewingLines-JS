4b78a9064499135240bf5a9b2d87ccfef7e7cdee
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-11-16 12:42:47 +0300

    Created array and saved state of random rectangles

--- index.js
+++ index.js
@@ -6,33 +6,43 @@ const settings = {
   dimensions: [1080, 1080],
 };
 
-const sketch = () => {
+const sketch = ({ context, width, height }) => {
   let x, y, w, h;
   //let angle, rx, ry;
   const num = 20;
+  const degrees = -30;
+
+  const rects = [];
+
+  for (let i = 0; i < num; i++) {
+      x = random.range(0, width);
+      y = random.range(0, height);
+      w = random.range(200,600);
+      h = random.range(40,200);
+
+      rects.push({ x, y, w, h });
+  }
 
   //render function
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
-/*
-for (let i = 0; i < num; i++) {
-    x = width * 0.5;
-    y = height * 0.5;
-    w = width * 0.6;
-    h = height * 0.1;
 
-    context.save();
-    context.translate(x, y);
-    context.strokeStyle = `blue`;
-   
-    drawSkewedRect({context});
-    context.stroke();
+    rects.forEach(rect =>{
+      const {x, y, w, h} = rect;
+
+      context.save();
+      context.translate(x, y);
+      context.strokeStyle = `blue`;
+    
+      drawSkewedRect({context, w, h, degrees});
+      context.stroke();
 
 
-    context.restore();
-    }
-    */
+      context.restore();
+  
+    
+    });
     
   };
 };
